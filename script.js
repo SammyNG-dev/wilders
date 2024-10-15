7dca2f8259d7ede3ac84649e34878630a1f1cad4
SammyNG-dev <sammy.nguyenvanbau@gmail.com>
2024-10-15 11:29:32 +0200

    add nex files and some lines in script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+console.log(chalk.underline.blue("Hello") + " " + chalk.red("Sammy"));
+console.log(chalk.yellow("Cédric"));
+console.log(chalk.blue("Nadir"));
+console.log(chalk.green("Lise"));
